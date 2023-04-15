5e3b62b5a7ae9662c2fa6603bde5b00ee9918eb0
Real1236 <arthurchan2001@live.com>
2023-04-14 21:26:11 -0400

    Solved LC#90

---
+++ leetcode/editor/en/90.subsets-ii.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode id=90 lang=python3
+#
+# [90] Subsets II
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = [[]]
+
+        def dfs(subset: List[int], counts: dict) -> None:
+            numbers = list(counts.keys())
+            for num in numbers:
+                new_subset = subset.copy()
+                new_subset.append(num)
+                res.append(new_subset)
+
+                if counts[num] > 1:
+                    counts[num] -= 1
+                else:
+                    counts.pop(num)
+                new_counts = counts.copy()
+                dfs(new_subset, new_counts)
+                if counts[num]:
+                    counts.pop(num)
+
+        dfs([], Counter(nums))
+        return res
+        
+# @lc code=end
+
