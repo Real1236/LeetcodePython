f475cef9a3e037929d2283406047dde808646d88
Real1236 <arthurchan2001@live.com>
2024-05-01 19:16:22 -0400

    optimized solution for lc#2348

--- leetcode/editor/en/2348.number-of-zero-filled-subarrays.py
+++ leetcode/editor/en/2348.number-of-zero-filled-subarrays.py
@@ -10,39 +10,14 @@ from typing import List
 
 class Solution:
     def zeroFilledSubarray(self, nums: List[int]) -> int:
-        # Find longest sequence of zeroes
-        longest = 0
-        count = 0
+        res, count = 0, 0
         for num in nums:
-            if not num:
-                count += 1
-                if count > longest:
-                    longest = count
-            else:
+            if num:
                 count = 0
-
-        # Create mapping num of zeroes to occurrences of subarrays
-        # 1 -> 1
-        # 2 -> 3 = 1 + 2 = map[1] + 2
-        # 3 -> 6 = 1 + 2 + 3 = map[2] + 3
-        # 4 -> 10 = 1 + 2 + 3 + 4 = map[3] + 4
-        map = [0]
-        for i in range(1, longest + 1):
-            map.append(map[i - 1] + i)
-
-        # Loop through nums and add occurrences
-        res = 0
-        count = 0
-        for num in nums:
-            if not num:
-                count += 1
                 continue
-            
-            res += map[count]
-            count = 0
-            
-        res += map[count]
-        count = 0
+
+            count += 1
+            res += count
         
         return res
 
